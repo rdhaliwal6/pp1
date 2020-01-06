25eb27828d7d4d9fcf32664f8e1da86a960808a9
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-01-06 14:43:49 -0500

    Created and added the index.php page.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Name: Rajpreet Dhaliwal
+ * Date: 1-6-20
+ * URl: https://github.com/rdhaliwal6/pp1.git
+ * Pair Program 1
+ */
+
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 1</title>
+</head>
+<body>
+<h1>Pair Program 1</h1>
+</body>
+</html>
